26a2a61dbb81e69b8e5b3352cff065393a2b404f
bencbartlett <benjamincbartlett@gmail.com>
2024-05-15 10:51:42 +0000

    Deploying to gh-pages from @ bencbartlett/bencbartlett.github.io@86492d482d68455e301bdf51c8d23807ba937ff6 🚀

--- assets/img/gyroled/resizer.py
+++ assets/img/gyroled/resizer.py
@@ -20,14 +20,14 @@ for filename in os.listdir(directory):
         scaled_filepath = os.path.join(scaled_directory, filename)
 
         # Use ffmpeg to scale the image and compress it
-        subprocess.run(['ffmpeg', '-i', filepath, '-vf', 'scale=-2:1080', '-compression_level', '10', scaled_filepath])
+        subprocess.run(['ffmpeg', '-y', '-i', filepath, '-vf', 'scale=-2:1080', '-compression_level', '10', scaled_filepath])
 
     # Check if the file is a .mov file
     elif filename.endswith('.mov'):
         # Prepare the new .mp4 filename in the scaled directory
-        mp4_filepath = os.path.join(scaled_directory, filename.split('.')[0] + '.mp4')
+        mp4_filepath = os.path.join(scaled_directory, filename.split('.')[0] + '_h264.mp4')
 
-        # Use ffmpeg to convert .mov to .mp4, ensure width is divisible by 2, scale it, and apply compression
-        subprocess.run(['ffmpeg', '-i', filepath, '-vf', 'scale=-2:1080', '-c:v', 'libx265', '-crf', '28', '-tag:v', 'hvc1', '-pix_fmt', 'yuv420p', mp4_filepath])
+        # Convert .mov to .mp4 using H.264, ensure width is divisible by 2, scale it, and apply compression
+        subprocess.run(['ffmpeg', '-y', '-i', filepath, '-vf', 'scale=-2:1080', '-c:v', 'libx264', '-crf', '23', mp4_filepath])
 
 print("Processing complete.")
